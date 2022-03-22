67cb89ecf11653ce941b04d241c282411a6ad551
Sean Unland <sdunland@gmail.com>
2022-03-22 14:07:18 -0400

    Working on backend of travel log

--- server/src/index.js
+++ server/src/index.js
@@ -2,18 +2,29 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const middlewares = require("./middlwares");
+const logs = require("./api/logs");
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 // middleware
 app.use(morgan("common"));
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN,
   })
 );
+app.use(express.json());
 
 // route
 app.get("/", (req, res) => {
@@ -21,6 +32,8 @@ app.get("/", (req, res) => {
     message: "Hello World",
   });
 });
+
+app.use("/api/logs", logs);
 
 // the middleware is being imported from middleware.js
 app.use(middlewares.notFound);
--- server/src/models/LogEntry.js
+++ server/src/models/LogEntry.js
@@ -1,46 +1,40 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const requiredNumber = {
   type: Number,
-  required: true,
+  //   required: true,
 };
 
-const logEntrySchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: String,
-    comments: String,
-    image: String,
-    rating: {
-      type: Number,
-      min: 0,
-      max: 10,
-      default: 0,
-    },
-    latitude: {
-      ...requiredNumber,
-      min: -90,
-      max: 90,
-    },
-
-    longitude: {
-      ...requiredNumber,
-      min: -180,
-      max: 180,
-    },
-    visitDate: {
-      required: true,
-      type: Date,
-    },
+const logEntrySchema = new Schema({
+  title: {
+    type: String,
+    //   required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  description: String,
+  comments: String,
+  image: String,
+  rating: {
+    type: Number,
+    min: 0,
+    max: 10,
+    default: 0,
+  },
+  latitude: {
+    ...requiredNumber,
+    min: -90,
+    max: 90,
+  },
+
+  longitude: {
+    ...requiredNumber,
+    min: -180,
+    max: 180,
+  },
+  visitDate: {
+    type: Date,
+  },
+});
 const LogEntry = mongoose.model("LogEntry", logEntrySchema);
 
 module.exports = LogEntry;
